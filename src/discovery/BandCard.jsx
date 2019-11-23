31cfdd9557efcd224f1d7636de53e920e1f3e2ee
Rachel Kawula <rkawula@users.noreply.github.com>
2019-11-22 21:51:12 -0800

    Find band (#11)
    * Added click-to-select logic for band cards.
    
    * Added Done on find band results.
    
    * Updated test to fix minor change to band name creation page.
    
    * Fixed broken test.

--- src/discovery/BandCard.jsx
+++ src/discovery/BandCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {
-  Card, Image, Icon,
+  Card, Image,
 } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { BAND } from '../util/props';
 
 
-export default function BandCard({ band }) {
+export default function BandCard({ band, onBandClick, active }) {
   return (
-    <Card>
+    <Card
+      onClick={() => onBandClick(band.bandName)}
+      raised={active}
+    >
       <Image src="https://react.semantic-ui.com/images/avatar/large/matthew.png" wrapped ui={false} />
       <Card.Content>
         <Card.Header>{band.bandName}</Card.Header>
@@ -19,17 +22,12 @@ export default function BandCard({ band }) {
           {band.description}
         </Card.Description>
       </Card.Content>
-      <Card.Content extra>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a>
-          <Icon name="user" />
-          22 Friends
-        </a>
-      </Card.Content>
     </Card>
   );
 }
 
 BandCard.propTypes = {
   band: PropTypes.shape(BAND).isRequired,
+  onBandClick: PropTypes.func.isRequired,
+  active: PropTypes.bool.isRequired,
 };
--- src/discovery/FindBand.jsx
+++ src/discovery/FindBand.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {
-  Grid, Dimmer, Loader,
+  Grid, Dimmer, Loader, Button,
 } from 'semantic-ui-react';
+import { Redirect } from 'react-router-dom';
+
 import BandGrid from './BandGrid';
 
 const FAKE_BANDS = [
   {
+    id: 1,
     bandName: 'The Wombats',
     bandRoles: ['Vocalist', 'Drums', 'Guitar', 'Bass'],
     emptyRoles: ['Keys', 'Wildcard'],
@@ -13,6 +16,7 @@ const FAKE_BANDS = [
     inspirations: ['The Strokes'],
   },
   {
+    id: 2,
     bandName: 'Keane',
     bandRoles: ['Vocalist', 'Drums', 'Keys', 'Bass'],
     emptyRoles: ['Guitar'],
@@ -20,6 +24,7 @@ const FAKE_BANDS = [
     inspirations: ['The Beatles', 'The Strokes'],
   },
   {
+    id: 3,
     bandName: 'Long Birds',
     bandRoles: ['Vocalist', 'Drums', 'Keys', 'Bass'],
     emptyRoles: ['Bass', 'Guitar', 'Guitar'],
@@ -33,6 +38,8 @@ export default class FindBand extends React.Component {
     super(props);
     this.state = {
       loading: false,
+      currentBand: '',
+      redirectHome: false,
     };
   }
 
@@ -47,25 +54,47 @@ export default class FindBand extends React.Component {
     }, 1000);
   }
 
-  render() {
-    const { loading } = this.state;
+  onBandClick = (bandName) => {
+    // TODO: Replace with band ID rather than string match on name.
+    const { currentBand } = this.state;
+    let selectedBand = bandName;
+    if (currentBand === selectedBand) {
+      selectedBand = '';
+    }
+    this.setState({
+      currentBand: selectedBand,
+    });
+  };
 
+  render() {
+    const { loading, currentBand, redirectHome } = this.state;
+
+    if (redirectHome) {
+      return (
+        <Redirect to="/" />
+      );
+    }
     return (
-      <Grid columns={2} relaxed="very" stackable>
-        <Dimmer
-          active={loading}
-        >
-          <Loader
-            size="massive"
+      <>
+        <Grid columns={2} relaxed="very" stackable>
+          <Dimmer
+            active={loading}
           >
-            Finding Bands...
-          </Loader>
-        </Dimmer>
-        <BandGrid
-          loading={loading}
-          bands={FAKE_BANDS}
-        />
-      </Grid>
+            <Loader
+              size="massive"
+            >
+              Finding Bands...
+            </Loader>
+          </Dimmer>
+          <BandGrid
+            loading={loading}
+            bands={FAKE_BANDS}
+            currentBand={currentBand}
+            onBandClick={this.onBandClick}
+          />
+        </Grid>
+        <Button onClick={() => this.setState({ redirectHome: true })}>Done</Button>
+      </>
     );
   }
 }
